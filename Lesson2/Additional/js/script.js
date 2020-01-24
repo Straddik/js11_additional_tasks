8dc043790466d991130f4c01278ce97690907fc5
Anastasiya Shkrogal <straddi@mail.ru>
2020-01-24 21:20:42 +0300

    Добавлена ; в 26 строку

--- Lesson2/Additional/js/script.js
+++ Lesson2/Additional/js/script.js
@@ -20,7 +20,7 @@ while (numOfDig < (num * 10)) {
 console.log("б) Произведение цифр числа num равно: " + product2);
 
 // 3) Вывод полученного результата в степень 3, не используя Math.pow 
-product = product ** 3
+product = product ** 3;
 console.log("Полученный результат в степени 3: " + product);
 // 4) Вывод на экран первых 2 цифр полученного числа
 product = String(product);
