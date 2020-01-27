428cf0d83ce124d42254a67f7b6365238e54256a
Anastasiya Shkrogal <straddi@mail.ru>
2020-01-27 14:51:11 +0300

    Доделано усложнённое задание урока 3

--- Lesson3/js/script.js
+++ Lesson3/js/script.js
@@ -1,12 +1,14 @@
 // 1) Через if:
-let lang = 'ru';
-let weekRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-let weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+let languages = ['ru', 'en'],
+    lang = languages[Math.floor((Math.random() * languages.length))],
+    weekRu = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    weekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 if (lang == 'ru') {
-    console.log(weekRu);
+    console.log(...weekRu);
 } else if (lang == 'en') {
-    console.log(weekEn);
+    console.log(...weekEn);
 } else {
     console.log("Ошибка выбора языка");
 }
@@ -15,49 +17,33 @@ if (lang == 'ru') {
 
 switch (lang) {
     case 'ru':
-        console.log(weekRu);
+        console.log(...weekRu);
         break;
     case 'en':
-        console.log(weekEn);
+        console.log(...weekEn);
         break;
     default:
         console.log("Ошибка выбора языка");
 }
-// 3) Через коллекцию:
 
-let daysOfWeek = {
+// 3)а Через массив объектов:
+
+let daysOfWeek = [{ ru: weekRu }, { en: weekEn }];
+daysOfWeek = daysOfWeek.filter(val => val[lang]);
+console.log(...daysOfWeek[0][lang]);
+
+// 3)б Через объект:
+
+daysOfWeek = {
     'ru': weekRu,
     'en': weekEn,
 }
+console.log(...daysOfWeek[lang]);
 
-console.log(daysOfWeek[lang]);
+//4) Через тернарный оператор:
+let names = ['Артём', 'Максим', 'Илья', ' Алексей', 'Матвей', 'Кирилл', 'Евгений'],
+    namePerson = names[Math.floor((Math.random() * names.length))];
 
-
-
-
-
-
-
-// Переменная lang может принимать 2 значения: 'ru' 'en'.
-
-// Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке. Решите задачу
-
-// через if, 
-
-// через switch-case 
-
-// через многомерный массив без ифов и switch.
-
-
-
-
-
-// У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
-
-// 	Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-
-
-
-
-
-// Запушить проект в репозиторий для усложненных заданий на GitHub
+namePerson == 'Артём' ? console.log(namePerson + ' - это директор') :
+    namePerson == 'Максим' ? console.log(namePerson + ' - это преподаватель') :
+    console.log(namePerson + ' - это студент');
