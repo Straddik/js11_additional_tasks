c3e1dd8bf972e9646f14c502470123d8808c2288
Anastasiya Shkrogal <straddi@mail.ru>
2020-01-31 20:39:15 +0300

    Добавила вывод на страницу(не поняла задание)

--- Lesson7/js/script.js
+++ Lesson7/js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 let day = new Date();
 
+
 const normalizeDayOfWeek = function(d) {
     if (d) {
         d = d - 1;
@@ -14,13 +15,19 @@ let dayOfWeek = normalizeDayOfWeek(day.getDay());
 
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 week.forEach((val, index, arr) => {
+    let div = document.createElement('div');
     if ((index === dayOfWeek) && (index === 5 || index === 6)) {
+        div.innerHTML = "<i><b>" + val + "</b></i>";
         console.log('%c' + val, "font-style: italic; font-weight: 900;");
     } else if (index === dayOfWeek) {
+        div.innerHTML = "<b>" + val + "</b>";
         console.log('%c' + val, "font-weight: 900;");
     } else if (index === 5 || index === 6) {
+        div.innerHTML = "<i>" + val + "</i>";
         console.log('%c' + val, "font-style: italic;");
     } else {
+        div.innerHTML = val;
         console.log(val);
     }
+    document.body.append(div);
 });
